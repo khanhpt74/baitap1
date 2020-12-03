37876132fe79448c52cf1e6489f7d2b2b7053286
khanhpt74 <72086102+khanhpt74@users.noreply.github.com>
2020-12-04 00:53:55 +0700

    Add files via upload

---
+++ baitap.py
@@ -0,0 +1,14 @@
+import random
+x = []
+x = random.randint(50, 1000)
+print(x)
+# Sinh ngẫu nhiên 1 List các số nguyên trong khoảng [-1000, 1000]
+import numpy as np
+x = np.random.randint(-1000, 1000, dtype = int)
+print(x)
+#Sinh ngẫu nhiên 1 List các số thực trong khoảng [-1000.0, 1000.0]
+import numpy as np
+x = np.random.uniform(-1000.0,1000.0)
+print(x)    
+    
+
